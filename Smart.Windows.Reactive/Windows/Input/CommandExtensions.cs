079d91a27901e86138aa3c84e3610b06802281bb
usausa <machi.pon@gmail.com>
2016-10-10 12:43:41 +0900

    Add CommandExtensions

---
+++ Smart.Windows.Reactive/Windows/Input/CommandExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Smart.Windows.Input
+{
+    using System;
+    using System.Reactive.Linq;
+    using System.Windows.Input;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static class CommandExtensions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public static IObservable<EventArgs> AsObservable(this ICommand command)
+        {
+            return Observable.FromEvent<EventHandler, EventArgs>(
+                h => (sender, e) => h(e),
+                h => command.CanExecuteChanged += h,
+                h => command.CanExecuteChanged -= h);
+        }
+    }
+}
