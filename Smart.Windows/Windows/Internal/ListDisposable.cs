f97c2f39e02b99748f162b6e9a55ba60da935cc4
usausa <machi.pon@gmail.com>
2017-09-21 14:27:14 +0900

    Update ViewModelBase

---
+++ Smart.Windows/Windows/Internal/ListDisposable.cs
@@ -0,0 +1,107 @@
+﻿namespace Smart.Windows.Internal
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///
+    /// </summary>
+    internal sealed class ListDisposable : ICollection<IDisposable>, IDisposable
+    {
+        private readonly List<IDisposable> disposables = new List<IDisposable>();
+
+        /// <summary>
+        ///
+        /// </summary>
+        public int Count => disposables.Count;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public bool IsReadOnly => false;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public void Dispose()
+        {
+            foreach (var disposable in disposables)
+            {
+                disposable.Dispose();
+            }
+
+            disposables.Clear();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<IDisposable> GetEnumerator()
+        {
+            return disposables.GetEnumerator();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(IDisposable item)
+        {
+            disposables.Add(item);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Remove(IDisposable item)
+        {
+            return disposables.Remove(item);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public void Clear()
+        {
+            foreach (var disposable in disposables)
+            {
+                disposable.Dispose();
+            }
+
+            disposables.Clear();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Contains(IDisposable item)
+        {
+            return disposables.Contains(item);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="arrayIndex"></param>
+        public void CopyTo(IDisposable[] array, int arrayIndex)
+        {
+            Array.Copy(disposables.ToArray(), 0, array, arrayIndex, array.Length - arrayIndex);
+        }
+    }
+}
--- Smart.Windows/Windows/ViewModels/ViewModelBase.cs
+++ Smart.Windows/Windows/ViewModels/ViewModelBase.cs
@@ -8,16 +8,25 @@
     using System.Threading.Tasks;
 
     using Smart.ComponentModel;
+    using Smart.Windows.Input;
+    using Smart.Windows.Internal;
     using Smart.Windows.Messaging;
 
     /// <summary>
     ///
     /// </summary>
-    public abstract class ViewModelBase : NotificationObject, IDataErrorInfo
+    public abstract class ViewModelBase : NotificationObject, IDataErrorInfo, IDisposable
     {
+        private ListDisposable disposables;
+
         private Messenger messenger;
 
         private bool isBusy;
+
+        /// <summary>
+        ///
+        /// </summary>
+        protected ICollection<IDisposable> Disposables => disposables ?? (disposables = new ListDisposable());
 
         /// <summary>
         ///
@@ -89,6 +98,35 @@
         protected ViewModelBase(Messenger messenger)
         {
             this.messenger = messenger;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        ~ViewModelBase()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                disposables?.Dispose();
+            }
         }
 
         // ------------------------------------------------------------
@@ -172,6 +210,154 @@
             {
                 IsBusy = false;
             }
+        }
+
+        // ------------------------------------------------------------
+        // Command helper
+        // ------------------------------------------------------------
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <returns></returns>
+        protected AsyncCommand MakeBusyCommand(Func<Task> execute)
+        {
+            return MakeBusyCommand(execute, Actions.True);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        /// <returns></returns>
+        protected AsyncCommand MakeBusyCommand(Func<Task> execute, Func<bool> canExecute)
+        {
+            return new AsyncCommand(
+                async () =>
+                {
+                    IsBusy = true;
+                    try
+                    {
+                        await execute();
+                    }
+                    finally
+                    {
+                        IsBusy = false;
+                    }
+                }, () => !IsBusy && canExecute())
+                .Observe(this, nameof(IsBusy))
+                .RemoveObserverBy(Disposables);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="TParameter"></typeparam>
+        /// <param name="execute"></param>
+        /// <returns></returns>
+        protected AsyncCommand<TParameter> MakeBusyCommand<TParameter>(Func<TParameter, Task> execute)
+        {
+            return MakeBusyCommand(execute, Actions<TParameter>.True);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="TParameter"></typeparam>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        /// <returns></returns>
+        protected AsyncCommand<TParameter> MakeBusyCommand<TParameter>(Func<TParameter, Task> execute, Func<TParameter, bool> canExecute)
+        {
+            return new AsyncCommand<TParameter>(
+                async parameter =>
+                {
+                    IsBusy = true;
+                    try
+                    {
+                        await execute(parameter);
+                    }
+                    finally
+                    {
+                        IsBusy = false;
+                    }
+                }, parameter => !IsBusy && canExecute(parameter))
+                .Observe(this, nameof(IsBusy))
+                .RemoveObserverBy(Disposables);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <returns></returns>
+        protected AsyncCommand MakeBusyCommand(Action execute)
+        {
+            return MakeBusyCommand(execute, Actions.True);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        /// <returns></returns>
+        protected AsyncCommand MakeBusyCommand(Action execute, Func<bool> canExecute)
+        {
+            return new AsyncCommand(
+                () =>
+                {
+                    IsBusy = true;
+                    try
+                    {
+                        execute();
+                    }
+                    finally
+                    {
+                        IsBusy = false;
+                    }
+                }, () => !IsBusy && canExecute())
+                .Observe(this, nameof(IsBusy))
+                .RemoveObserverBy(Disposables);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="TParameter"></typeparam>
+        /// <param name="execute"></param>
+        /// <returns></returns>
+        protected AsyncCommand<TParameter> MakeBusyCommand<TParameter>(Action<TParameter> execute)
+        {
+            return MakeBusyCommand(execute, Actions<TParameter>.True);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="TParameter"></typeparam>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        /// <returns></returns>
+        protected AsyncCommand<TParameter> MakeBusyCommand<TParameter>(Action<TParameter> execute, Func<TParameter, bool> canExecute)
+        {
+            return new AsyncCommand<TParameter>(
+                parameter =>
+                {
+                    IsBusy = true;
+                    try
+                    {
+                        execute(parameter);
+                    }
+                    finally
+                    {
+                        IsBusy = false;
+                    }
+                }, parameter => !IsBusy && canExecute(parameter))
+                .Observe(this, nameof(IsBusy))
+                .RemoveObserverBy(Disposables);
         }
     }
 }
