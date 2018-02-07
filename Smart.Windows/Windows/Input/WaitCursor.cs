df38e20a16a37ead1c59e1d0cbe116f1dea79c97
usausa <machi.pon@gmail.com>
2018-02-07 16:22:45 +0900

    Add wait cursor

---
+++ Smart.Windows/Windows/Input/WaitCursor.cs
@@ -0,0 +1,26 @@
+﻿namespace Smart.Windows.Input
+{
+    using System;
+    using System.Windows.Input;
+
+    public sealed class WaitCursor : IDisposable
+    {
+        private readonly Cursor oldCursor;
+
+        public WaitCursor()
+            : this(Cursors.Wait)
+        {
+        }
+
+        public WaitCursor(Cursor cursor)
+        {
+            oldCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = cursor;
+        }
+
+        public void Dispose()
+        {
+            Mouse.OverrideCursor = oldCursor;
+        }
+    }
+}
