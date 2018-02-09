abf2f371aafcaf20f41e3fd1b8c0b2fbfc284902
usausa <machi.pon@gmail.com>
2018-02-09 19:39:39 +0900

    Add Aactions

---
+++ Smart.Windows/Windows/Interactivity/ApplyEffectAction.cs
@@ -0,0 +1,40 @@
+﻿namespace Smart.Windows.Interactivity
+{
+    using System.Windows;
+    using System.Windows.Interactivity;
+    using System.Windows.Media.Effects;
+
+    /// <summary>
+    ///
+    /// </summary>
+    [TypeConstraint(typeof(FrameworkElement))]
+    public sealed class ApplyEffectAction : TriggerAction<FrameworkElement>
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public static readonly DependencyProperty EffectProperty = DependencyProperty.Register(
+            nameof(Effect),
+            typeof(Effect),
+            typeof(ApplyEffectAction),
+            new PropertyMetadata(null));
+
+        /// <summary>
+        ///
+        /// </summary>
+        public Effect Effect
+        {
+            get => (Effect)GetValue(EffectProperty);
+            set => SetValue(EffectProperty, value);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="parameter"></param>
+        protected override void Invoke(object parameter)
+        {
+            AssociatedObject.Effect = parameter as Effect ?? Effect;
+        }
+    }
+}
