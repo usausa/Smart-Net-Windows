c94fed554b8f0a422cd5c2bf43b68466528e8494
usausa <machi.pon@gmail.com>
2018-02-10 19:09:59 +0900

    Update behaviors

---
+++ Smart.Windows/Windows/Interactivity/EnterKeyToCommandBehavior.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Smart.Windows.Interactivity
+{
+    public sealed class EnterKeyToCommandBehavior : Behavior<FrameworkElement>
+    {
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+            nameof(Command),
+            typeof(ICommand),
+            typeof(EnterKeyToCommandBehavior),
+            new PropertyMetadata(null));
+
+        public static readonly DependencyProperty CommandParameterProperty = DependencyProperty.Register(
+            nameof(CommandParameter),
+            typeof(object),
+            typeof(EnterKeyToCommandBehavior),
+            new PropertyMetadata(null));
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public object CommandParameter
+        {
+            get => GetValue(CommandParameterProperty);
+            set => SetValue(CommandParameterProperty, value);
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.KeyDown += OnKeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.KeyDown -= OnKeyDown;
+
+            base.OnDetaching();
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Command == null)
+            {
+                return;
+            }
+
+            if (e.Key == Key.Enter)
+            {
+                if (Command.CanExecute(CommandParameter))
+                {
+                    Command.Execute(CommandParameter);
+                }
+
+                e.Handled = true;
+            }
+        }
+    }
+}
