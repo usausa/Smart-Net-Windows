9905126856afa5f6001e00e2a6534198638df422
usausa <machi.pon@gmail.com>
2018-02-10 18:51:40 +0900

    Add behaviors

---
+++ Smart.Windows/Windows/Interactivity/SelectAllTextOnFocusBehavior.cs
@@ -0,0 +1,41 @@
+﻿namespace Smart.Windows.Interactivity
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Interactivity;
+
+    [TypeConstraint(typeof(TextBox))]
+    public sealed class SelectAllTextOnFocusBehavior : Behavior<TextBox>
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.GotKeyboardFocus += SelectAllText;
+            AssociatedObject.GotMouseCapture += SelectAllText;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.GotKeyboardFocus -= SelectAllText;
+            AssociatedObject.GotMouseCapture -= SelectAllText;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectAllText(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.SelectAll();
+        }
+    }
+}
---
+++ Smart.Windows/Windows/Interactivity/SetFocusOnLoadBehavior.cs
@@ -0,0 +1,24 @@
+﻿namespace Smart.Windows.Interactivity
+{
+    using System.Windows;
+    using System.Windows.Input;
+    using System.Windows.Interactivity;
+
+    /// <summary>
+    ///
+    /// </summary>
+    [TypeConstraint(typeof(UIElement))]
+    public sealed class SetFocusOnLoadBehavior : Behavior<UIElement>
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            var fs = FocusManager.GetFocusScope(AssociatedObject);
+            FocusManager.SetFocusedElement(fs, AssociatedObject);
+        }
+    }
+}
