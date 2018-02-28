26a6165fdda1d5ddc8df3cb8b580256bf92b881d
usausa <machi.pon@gmail.com>
2018-02-28 17:20:36 +0900

    Add text converter

---
+++ Smart.Windows/Windows/Data/BoolToTextConverter.cs
@@ -0,0 +1,59 @@
+﻿namespace Smart.Windows.Data
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    /// <summary>
+    ///
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(string))]
+    public sealed class BoolToTextConverter : IValueConverter
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public string TrueText { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public string FalseText { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (bool)value ? TrueText : FalseText;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Equals(value, TrueText))
+            {
+                return true;
+            }
+
+            if (Equals(value, FalseText))
+            {
+                return false;
+            }
+
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ Smart.Windows/Windows/Data/NullToTextConverter.cs
@@ -0,0 +1,49 @@
+﻿namespace Smart.Windows.Data
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    /// <summary>
+    ///
+    /// </summary>
+    [ValueConversion(typeof(object), typeof(string))]
+    public sealed class NullToTextConverter : IValueConverter
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public string NullText { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public string NonNullText { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? NullText : NonNullText;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
