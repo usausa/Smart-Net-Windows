9b61a4151838a04509647398fc20c6be5637e689
usausa <machi.pon@gmail.com>
2018-12-09 13:26:24 +0900

    Add converter

---
+++ Smart.Windows/Windows/Data/ColorBlendConverter.cs
@@ -0,0 +1,48 @@
+﻿namespace Smart.Windows.Data
+{
+    using System;
+    using System.Drawing;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(Color), typeof(Color))]
+    public sealed class ColorBlendConverter : IValueConverter
+    {
+        private double raito;
+
+        public Color BlendColor { get; set; }
+
+        public double Raito
+        {
+            get => raito;
+            set
+            {
+                if ((value < 0d) || (value > 1d))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+
+                raito = value;
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Color))
+            {
+                return null;
+            }
+
+            var color = (Color)value;
+            var r = Math.Min((byte)Math.Round(color.R + ((BlendColor.R - color.R) * raito)), (byte)255);
+            var g = Math.Min((byte)Math.Round(color.G + ((BlendColor.G - color.G) * raito)), (byte)255);
+            var b = Math.Min((byte)Math.Round(color.B + ((BlendColor.B - color.B) * raito)), (byte)255);
+            return Color.FromArgb(r, g, b);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
