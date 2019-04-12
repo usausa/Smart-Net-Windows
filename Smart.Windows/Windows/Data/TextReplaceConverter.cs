71c9d6bd1b2b66c50273e644b034b46ebca8a2c8
usausa <machi.pon@gmail.com>
2019-04-12 17:17:04 +0900

    Add converter

---
+++ Smart.Windows/Windows/Data/TextReplaceConverter.cs
@@ -0,0 +1,36 @@
+namespace Smart.Windows.Data
+{
+    using System;
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(string), typeof(string))]
+    public sealed class TextReplaceConverter : IValueConverter
+    {
+        public string Pattern { get; set; }
+
+        public string Replacement { get; set; }
+
+        public RegexOptions Options { get; set; }
+
+        public bool ReplaceAll { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string;
+            if (String.IsNullOrEmpty(str))
+            {
+                return value;
+            }
+
+            var regex = new Regex(Pattern, Options);
+            return regex.Replace(str, Replacement ?? string.Empty, ReplaceAll ? -1 : 1);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
