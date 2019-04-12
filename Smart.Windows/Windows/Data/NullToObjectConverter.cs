c10cf72cc37663cb06e93b053aa29101d6baf34e
usausa <machi.pon@gmail.com>
2019-04-12 17:52:02 +0900

    Update converter

---
+++ Smart.Windows/Windows/Data/NullToObjectConverter.cs
@@ -0,0 +1,73 @@
+namespace Smart.Windows.Data
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Media;
+
+    public class NullToObjectConverter<T> : IValueConverter
+    {
+        public T NullValue { get; set; }
+
+        public T NonNullValue { get; set; }
+
+        public bool HandleEmptyString { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((value is null) ||
+                (HandleEmptyString && String.IsNullOrEmpty(value as string)))
+            {
+                return NullValue;
+            }
+
+            return NonNullValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    [ValueConversion(typeof(object), typeof(bool))]
+    public sealed class NullToBooleanConverter : NullToObjectConverter<bool>
+    {
+        public NullToBooleanConverter()
+        {
+            NullValue = false;
+            NonNullValue = true;
+        }
+    }
+
+    [ValueConversion(typeof(object), typeof(string))]
+    public sealed class NullToTextConverter : NullToObjectConverter<string>
+    {
+    }
+
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public sealed class NullToVisibilityConverter : NullToObjectConverter<Visibility>
+    {
+    }
+
+    [ValueConversion(typeof(object), typeof(Brush))]
+    public sealed class NullToBrushConverter : NullToObjectConverter<Brush>
+    {
+        public NullToBrushConverter()
+        {
+            NullValue = Brushes.Transparent;
+            NonNullValue = Brushes.Transparent;
+        }
+    }
+
+    [ValueConversion(typeof(object), typeof(Color))]
+    public sealed class NullToColorConverter : NullToObjectConverter<Color>
+    {
+        public NullToColorConverter()
+        {
+            NullValue = Colors.Transparent;
+            NonNullValue = Colors.Transparent;
+        }
+    }
+}
