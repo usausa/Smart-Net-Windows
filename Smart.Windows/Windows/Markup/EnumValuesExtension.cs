4e70988bfbb8c3b7d07850e32d03b74fa72ec536
usausa <machi.pon@gmail.com>
2019-04-15 16:33:24 +0900

    Update extension

---
+++ Smart.Windows/Windows/Markup/EnumValuesExtension.cs
@@ -0,0 +1,20 @@
+namespace Smart.Windows.Markup
+{
+    using System;
+    using System.Collections;
+    using System.Windows.Markup;
+
+    [ContentProperty("Type")]
+    [MarkupExtensionReturnType(typeof(IEnumerable))]
+    public sealed class EnumValuesExtension : MarkupExtension
+    {
+        public Type Type { get; set; }
+
+        public EnumValuesExtension(Type type)
+        {
+            Type = type;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => Enum.GetValues(Type);
+    }
+}
