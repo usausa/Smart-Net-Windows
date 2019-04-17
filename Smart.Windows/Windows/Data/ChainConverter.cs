c1026b3a62bd9321725529b2ad626585ef03ca42
usausa <machi.pon@gmail.com>
2019-04-17 17:07:11 +0900

    Update converter

--- Smart.Windows/Windows/Data/ChainConverter.cs
+++ Smart.Windows/Windows/Data/ChainConverter.cs
@@ -10,18 +10,28 @@ namespace Smart.Windows.Data
 
     [ContentProperty("Converters")]
     [ValueConversion(typeof(object), typeof(object))]
-    public sealed class AggregateConverter : IValueConverter
+    public sealed class ChainConverter : IValueConverter
     {
         public Collection<IValueConverter> Converters { get; } = new Collection<IValueConverter>(new List<IValueConverter>());
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return Converters.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+            foreach (var converter in Converters)
+            {
+                value = converter.Convert(value, targetType, parameter, culture);
+            }
+
+            return value;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return Converters.Reverse().Aggregate(value, (current, converter) => converter.ConvertBack(current, targetType, parameter, culture));
+            foreach (var converter in Converters.Reverse())
+            {
+                value = converter.ConvertBack(value, targetType, parameter, culture);
+            }
+
+            return value;
         }
     }
 }
