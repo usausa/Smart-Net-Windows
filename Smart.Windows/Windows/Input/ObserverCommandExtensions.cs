8048f354631e4585dccaed8ffae86cd2457b5521
usausa <machi.pon@gmail.com>
2019-10-26 22:02:09 +0900

    Update extensions

--- Smart.Windows/Windows/Input/ObserverCommandExtensions.cs
+++ Smart.Windows/Windows/Input/ObserverCommandExtensions.cs
@@ -2,13 +2,13 @@ namespace Smart.Windows.Input
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
 
-    using Smart.ComponentModel;
     using Smart.Windows.Internal;
 
     public static class ObserverCommandExtensions
     {
-        public static TCommand Observe<TCommand, TValue>(this TCommand command, NotificationValue<TValue> value)
+        public static TCommand Observe<TCommand>(this TCommand command, params INotifyPropertyChanged[] values)
             where TCommand : ObserveCommandBase<TCommand>
         {
             if (command is null)
@@ -16,12 +16,38 @@ namespace Smart.Windows.Input
                 throw new ArgumentNullException(nameof(command));
             }
 
-            if (value is null)
+            if (values is null)
             {
-                throw new ArgumentNullException(nameof(value));
+                throw new ArgumentNullException(nameof(values));
             }
 
-            return command.Observe(value, nameof(value.Value));
+            foreach (var value in values)
+            {
+                command.Observe(value);
+            }
+
+            return command;
+        }
+
+        public static TCommand Observe<TCommand>(this TCommand command, IEnumerable<INotifyPropertyChanged> values)
+            where TCommand : ObserveCommandBase<TCommand>
+        {
+            if (command is null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            if (values is null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            foreach (var value in values)
+            {
+                command.Observe(value);
+            }
+
+            return command;
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:DisposeObjectsBeforeLosingScope", Justification = "Ignore")]
