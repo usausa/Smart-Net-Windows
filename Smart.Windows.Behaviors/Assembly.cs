364936b0bf0039916a8638e05e49aa770834517f
usausa <machi.pon@gmail.com>
2025-07-13 19:56:50 +0900

    Update behaviors

--- Smart.Windows.Behaviors/Assembly.cs
+++
@@ -1,4 +0,0 @@
-[assembly: CLSCompliant(false)]
-
-[assembly: System.Windows.Markup.XmlnsDefinition("http://smart/windows", "Smart.Windows.Interactivity")]
-[assembly: System.Windows.Markup.XmlnsPrefix("http://smart/windows", "s")]
--- Smart.Windows/Assembly.cs
+++ Smart.Windows/Assembly.cs
@@ -2,5 +2,7 @@
 
 [assembly: System.Windows.Markup.XmlnsDefinition("http://smart/windows", "Smart.Windows")]
 [assembly: System.Windows.Markup.XmlnsDefinition("http://smart/windows", "Smart.Windows.Data")]
+[assembly: System.Windows.Markup.XmlnsDefinition("http://smart/windows", "Smart.Windows.Expressions")]
+[assembly: System.Windows.Markup.XmlnsDefinition("http://smart/windows", "Smart.Windows.Interactivity")]
 [assembly: System.Windows.Markup.XmlnsDefinition("http://smart/windows", "Smart.Windows.Markup")]
 [assembly: System.Windows.Markup.XmlnsPrefix("http://smart/windows", "s")]
