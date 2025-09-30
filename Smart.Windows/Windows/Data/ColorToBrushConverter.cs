631eef27d3f7340365477ede4e027338dabc1072
usausa <machi.pon@gmail.com>
2025-09-30 09:38:24 +0900

    Update converter

---
+++ Smart.Windows/Windows/Data/ColorToBrushConverter.cs
@@ -0,0 +1,20 @@
+namespace Smart.Windows.Data;
+
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+#pragma warning disable IDE0032
+[ValueConversion(typeof(Color), typeof(SolidColorBrush))]
+public sealed class ColorToBrushConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is Color color ? new SolidColorBrush(color) : null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is SolidColorBrush brush ? brush.Color : null;
+    }
+}
